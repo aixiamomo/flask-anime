f0e2f919cc56e158ad4a336aa0d1aed4d6e1a228
aixiamomo <920534583@qq.com>
2016-10-18 13:16:23 +0800

    基于 Flask 与 MySQL 实现番剧推荐系统

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 # -*- coding:utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template, request
+import recommend
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello'
+    return render_template('index.html')
+
+
+@app.route('/search/')
+def search():
+    # request为全局变量，可得到用户输入信息
+    n = request.args.get('user')
+    # 调用推荐函数
+    dic = recommend.recommend(n)
+    # 用返回的结果渲染模板
+    return render_template('search.html', Data=dic)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-sql = '''
-select style_id from
-'''
--- recommend.py
+++ recommend.py
@@ -4,48 +4,80 @@ import MySQLdb
 
 
 def recommend(user):
-    # 连接数据库
-    DB = MySQLdb.connect("localhost", "root", "qq920534583", "recommend")
-    # 获取游标
-    c = DB.cursor()
+    try:
+        # 连接数据库
+        DB = MySQLdb.connect("localhost", "root", "qq920534583", "recommend")
+        # 获取游标
+        c = DB.cursor()
 
-    # 下面代码实现从数据库中得到用户user喜欢的番剧编号，以便避免重复
-    love = []
-    # sql语句
-    sql = "SELECT anime_id FROM user_anime WHERE user_id=%s;"
-    c.execute(sql, str(user))
-    # 得到结果集，接收全部的返回结果行
-    results = c.fetchall()
-    for line in results:
-        love.append(line[0])
+        # 下面代码实现从数据库中得到用户user喜欢的番剧编号，以便避免重复
+        love = []
+        # sql语句
+        sql = "SELECT anime_id FROM user_anime WHERE user_id=%s;"
+        c.execute(sql, str(user))
+        # 得到结果集，接收全部的返回结果行
+        results = c.fetchall()
+        for line in results:
+            love.append(line[0])
 
-    # 下面代码实现得到用户喜欢的top3类型
-    sql = '''
-    select style_id from
-        (select user_id,style_id from
-        (select user_id,anime_id as id from user_anime where user_id=%s) as s
-        natural join anime natural join
-        (select anime_id as id,style_id from anime_style) as n
-         )as temp group by style_id order by count(user_id) desc limit 3;
-         '''
-    c.execute(sql, str(user))
-    results = c.fetchall()
-    lis = []
-    anime = {}
-    for line in results:
-        lis.append(line[0])
+        # 下面代码实现得到用户喜欢的top3类型
+        sql = '''
+        select style_id from
+            (select user_id,style_id from
+            (select user_id,anime_id as id from user_anime where user_id=%s) as s
+            natural join anime natural join
+            (select anime_id as id,style_id from anime_style) as n
+             )as temp group by style_id order by count(user_id) desc limit 3;
+             '''
+        c.execute(sql, str(user))
+        results = c.fetchall()
+        lis = []
+        anime = {}
+        for line in results:
+            lis.append(line[0])
 
-    # 从番剧信息的数据库anime_style中得到top3各个类别的所有番剧并存到anime字典中
-    for i in lis:
-        sql = "SELECT anime_id FROM anime_style WHERE style_id={};".format(i)
+        # 从番剧信息的数据库anime_style中得到top3各个类别的所有番剧并存到anime字典中
+        for i in lis:
+            sql = "SELECT anime_id FROM anime_style WHERE style_id={};".format(i)
+            c.execute(sql)
+            results = c.fetchall()
+            anime_lis = []
+            for result in results:
+                anime_lis.append(result[0])
+            # 类型为key，值为存放番剧数据的列表
+            anime[str(i)] = anime_lis
+        # 建立三个类别番组的set(集合），并取交集，即得到同时具有三个类型标签的番剧
+        s = set(anime[str(lis[0])]) & set(anime[str(lis[1])]) & set(anime[str(lis[2])])
+
+        # 建立用户喜欢番剧的集合
+        loveset = set(love)
+
+        # 如果系统得到的所有番剧用户都已看过，即loveset>s，就从TOP1类型即最喜欢的类型里挑选一个
+        if loveset > s:
+            s = set(anime[str(list[0])])
+
+        # 把集合转换成列表提供给随机函数使用
+        set_lis = []
+        for i in s:
+            set_lis.append(i)
+
+        # 从结果中随机挑选
+        recommendation = choice(set_lis)
+
+        # 直到挑选到用户没看过的，（上文有个if判断，所以这里set_lis>love，不会陷入死循环，）
+        while recommendation in love:
+            recommendation = choice(set_lis)
+        dic = {}
+
+        # 得到番剧的相关信息
+        sql = "SELECT name,brief FROM anime WHERE id={};".format(recommendation)
         c.execute(sql)
         results = c.fetchall()
-        anime_lis = []
-        for result in results:
-            anime_lis.append(result[0])
-        # 类型为key，值为存放番剧数据的列表
-        anime[str(i)] = anime_lis
-    print anime
-    c.close()
+        dic['name'] = results[0][0]
+        dic['brief'] = results[0][1]
+    finally:
+        # 关闭指针和连接
+        c.close()
+        DB.close()
+        return dic
 
-recommend(1)
